ecba224d532e289adc7fba1444e45c2a552526f4
Markus <markus@Markuss-Mac.local>
2014-11-13 14:54:35 +0100

    create tableview

--- MAD_Uebung2/VC_ManageArtist.swift
+++ MAD_Uebung2/VC_ManageArtist.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  VC_ManageArtist.swift
 //  MAD_Uebung2
 //
 //  Created by Markus on 11/13/14.
@@ -9,20 +9,19 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class VC_DisplayArtist: UIViewController {
     
     var artist = [NSManagedObject]();
-    var album = [NSManagedObject]();
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
